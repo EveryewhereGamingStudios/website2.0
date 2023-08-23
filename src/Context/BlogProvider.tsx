01641790f6604dcc777def325ddd634415991ec7
heluansantos <heluan.caetano@gmail.com>
2023-08-24 00:55:05 +0200

    feat: Blog improvments

--- src/Context/BlogProvider.tsx
+++ src/Context/BlogProvider.tsx
@@ -4,96 +4,94 @@ import { collection, getDoc, getDocs, doc } from "firebase/firestore";
 // import blog from "../data/blog.json";
 
 interface Article {
-    id: string;
-    title: string;
-    content: string;
-    summary: string;
-    image: string;
-    tags: string[];
-    date: {
-        seconds: number;
-    }
+  id: string;
+  title: string;
+  content: string;
+  summary: string;
+  image: string;
+  tags: string[];
+  date: {
+    seconds: number;
+  };
 }
 
 interface BlogContextProps {
-    articles: Article[];
-    article: Article | undefined;
-    loading: boolean;
-    error: string;
-    getArticleById: (id: string) => Promise<Article | undefined>;
-    getArticles: () => Promise<Article[]>;
+  articles: Article[];
+  article: Article | undefined;
+  loading: boolean;
+  error: string;
+  getArticleById: (id: string) => Promise<Article | undefined>;
+  getArticles: () => Promise<Article[]>;
 }
 
-export const BlogContext = createContext<BlogContextProps>({} as BlogContextProps);
+export const BlogContext = createContext<BlogContextProps>(
+  {} as BlogContextProps
+);
 
-const BlogProvider = ({children}: any) => {
-    const [articles, setArticles] = useState<Article[]>([]);
-    const [article, setArticle] = useState<Article | undefined>(undefined);
-    const [loading, setLoading] = useState<boolean>(false);
-    const [error, setError] = useState<string>("");
-    const {db} = useContext(FirebaseContext);
-    const blogRef = collection(db, 'blog');
+const BlogProvider = ({ children }: any) => {
+  const [articles, setArticles] = useState<Article[]>([]);
+  const [article, setArticle] = useState<Article | undefined>(undefined);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string>("");
+  const { db } = useContext(FirebaseContext);
+  const blogRef = collection(db, "blog");
 
-    const getArticleById = async (id: string) => {
-        setLoading(true);
+  const getArticleById = async (id: string) => {
+    setLoading(true);
 
-        try {   
-            const docRef = await doc(db, "blog", id);
-            const docSnap = await getDoc(docRef);
+    try {
+      const docRef = doc(db, "blog", id);
+      const docSnap = await getDoc(docRef);
 
-            setArticle({
-                ...docSnap.data() as Article,
-                id: docSnap.id as string,
-            });
-
-
-        } finally {
-            setLoading(false);
-        }
-
-        return undefined;
+      setArticle({
+        ...(docSnap.data() as Article),
+        id: docSnap.id as string,
+      });
+    } finally {
+      setLoading(false);
     }
 
-    const getArticles = async () => {
-        setLoading(true);
+    return undefined;
+  };
 
-        
+  const getArticles = async () => {
+    setLoading(true);
 
-        try {
-        
-            const querySnapshot = await getDocs(blogRef);
-            querySnapshot.forEach((doc: any) => {
-
-                setArticles([
-                    ...articles,
-                    {
-                        ...doc.data() as Article,
-                        id: doc.id as string,
-                    }
-                ])
-            });
-    
-        } catch (e:any) {
-            setError(e + "");
-        }finally {
-            setLoading(false);
-        } 
-
-        return [];
+    try {
+      const querySnapshot = await getDocs(blogRef);
+      querySnapshot.forEach((doc: any) => {
+        setArticles([
+          ...articles,
+          {
+            ...(doc.data() as Article),
+            id: doc.id as string,
+          },
+        ]);
+      });
+    } catch (e: any) {
+      setError(e + "");
+    } finally {
+      setLoading(false);
     }
 
+    return [];
+  };
 
-    return <BlogContext.Provider value={{
+  return (
+    <BlogContext.Provider
+      value={{
         articles,
         article,
         loading,
         error,
         getArticleById,
-        getArticles
-    }}> {children} </BlogContext.Provider>
-}
+        getArticles,
+      }}
+    >
+      {" "}
+      {children}{" "}
+    </BlogContext.Provider>
+  );
+};
 
 export default BlogProvider;
-        
-
-        
--- src/Pages/Blog.tsx
+++ src/Pages/Blog.tsx
@@ -1,42 +1,45 @@
-import { useContext, useEffect } from 'react';
-import { BlogContext } from '../Context/BlogProvider';
+import { useContext, useEffect } from "react";
+import { BlogContext } from "../Context/BlogProvider";
 
 const Blog = () => {
-    const { articles, loading, error, getArticles } = useContext(BlogContext);
+  const { articles, loading, error, getArticles } = useContext(BlogContext);
 
-    useEffect(() => {
-        
-        // console.log(">>>>>")
-        getArticles();
+  useEffect(() => {
+    getArticles();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-        // console.log({
-        //     articles,
-        //     loading,
-        //     error,
-        //     getArticles
-        // })
-
-    },[]);
-
-    return <>
-        <div className="blog">
-            {loading && <p className="loading">Loading...</p>}
-            {error && <p className="error">{error}</p>}
-            {articles.map(article => {
-                const date = article.date ? new Date(article.date.seconds * 1000) : new Date();
-                return <div className="article" key={article.id}>
-                    <img src={article.image} alt={article.title} className='' />
-                    <h2 className="title">
-                        <a href={`/blog/${article.id}`}>
-                            {article.title}
-                        </a>
-                    </h2>
-                    <p className="summary">{article.summary} ...</p>
-                    <p className="date">{date.toDateString()}</p>
+  return (
+    <>
+      <div className="blog">
+        {loading && <p className="loading">Loading...</p>}
+        {error && <p className="error">{error}</p>}
+        {articles?.map((article) => {
+          const date = article.date
+            ? new Date(article.date.seconds * 1000)
+            : new Date();
+          return (
+            <div className="article" key={article.id}>
+              <img
+                src={article.image || "/assets/images/space.png"}
+                alt={article.title}
+              />
+              <div className="card_article">
+                <a href={`/blog/${article.id}`}>
+                  <h2 className="title">{article.title} </h2>
+                </a>
+                <p className="summary">{article.summary.slice(0, 120)} ...</p>
+                <div className="card-bottom">
+                  <p className="time">3 min</p>
+                  <p className="date">{date.toDateString()}</p>
                 </div>
-            })}
-        </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </>
-}
+  );
+};
 
 export default Blog;
