691279c12f79078ae2cf01db8b725ffde7e56de1
heluansantos <heluan.caetano@gmail.com>
2023-08-23 21:45:21 +0200

    feat: Security improvements

--- src/Context/FirebaseProvider.tsx
+++ src/Context/FirebaseProvider.tsx
@@ -1,50 +1,67 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useState } from "react";
 import { FirebaseApp, initializeApp } from "firebase/app";
 import { Analytics, getAnalytics } from "firebase/analytics";
-import { Auth, getAuth, GoogleAuthProvider, signInWithPopup, UserCredential } from "firebase/auth";
+import {
+  Auth,
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+} from "firebase/auth";
 import { Firestore, getFirestore } from "firebase/firestore";
-import firebaseConfig from '../Config/firebase.json';
 
 interface FirebaseContextProps {
-    app: FirebaseApp;
-    db: Firestore;
-    analytics: Analytics;
-    auth: Partial<Auth>;
-    provider: Partial<GoogleAuthProvider>;
-    authenticate: any
+  app: FirebaseApp;
+  db: Firestore;
+  analytics: Analytics;
+  auth: Partial<Auth>;
+  provider: Partial<GoogleAuthProvider>;
+  authenticate: any;
 }
 
-export const FirebaseContext = createContext<FirebaseContextProps>({} as FirebaseContextProps);
+export const FirebaseContext = createContext<FirebaseContextProps>(
+  {} as FirebaseContextProps
+);
 
 const FirebaseProvider = ({ children }: any) => {
-    const [app, setApp] = useState(initializeApp(firebaseConfig));
-    const [db, setDb] = useState(getFirestore(app));
-    const [analytics, setAnalytics] = useState(getAnalytics(app));
-    const [auth, setAuth] = useState(getAuth(app));
-    const [googleAuthProvider, setGoogleAuthProvider] = useState(new GoogleAuthProvider());
+  const [app] = useState(
+    initializeApp({
+      apiKey: process.env.REACT_APP_API_KEY,
+      authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+      projectId: process.env.REACT_APP_PROJECT_ID,
+      storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+      messagingSenderId: process.env.REACT_APP_MESSAGING_SENDR_ID,
+      appId: process.env.REACT_APP_APP_ID,
+      measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+    })
+  );
+  const [db] = useState(getFirestore(app));
+  const [analytics] = useState(getAnalytics(app));
+  const [auth] = useState(getAuth(app));
+  const [googleAuthProvider] = useState(new GoogleAuthProvider());
 
-    const authenticate = async () => {
-        googleAuthProvider.addScope('profile');
+  const authenticate = async () => {
+    googleAuthProvider.addScope("profile");
 
-        try {
-
-            await signInWithPopup(auth, googleAuthProvider);
-            
-        
-        } catch(e) {
-            console.error(e);
-        }
+    try {
+      await signInWithPopup(auth, googleAuthProvider);
+    } catch (e) {
+      console.error(e);
     }
-    return <FirebaseContext.Provider value={{
+  };
+  return (
+    <FirebaseContext.Provider
+      value={{
         app,
         db,
         analytics,
         auth,
         provider: googleAuthProvider,
-        authenticate
-    }}>
-        {children}
+        authenticate,
+      }}
+    >
+      {children}
     </FirebaseContext.Provider>
-}
+  );
+};
 
 export default FirebaseProvider;
