8517e90953d7d42605b1f3bd8c1adeda07f2e539
heluansantos <heluan.caetano@gmail.com>
2023-08-24 15:47:36 +0200

    feat: Blog cards with tailwind CSS

--- src/Pages/Blog.tsx
+++ src/Pages/Blog.tsx
@@ -10,35 +10,37 @@ const Blog = () => {
   }, []);
 
   return (
-    <>
-      <div className="blog">
-        {loading && <p className="loading">Loading...</p>}
-        {error && <p className="error">{error}</p>}
-        {articles?.map((article) => {
-          const date = article.date
-            ? new Date(article.date.seconds * 1000)
-            : new Date();
-          return (
-            <div className="article" key={article.id}>
-              <img
-                src={article.image || "/assets/images/space.png"}
-                alt={article.title}
-              />
-              <div className="card_article">
-                <a href={`/blog/${article.id}`}>
-                  <h2 className="title">{article.title} </h2>
-                </a>
-                <p className="summary">{article.summary.slice(0, 120)} ...</p>
-                <div className="card-bottom">
-                  <p className="time">3 min</p>
-                  <p className="date">{date.toDateString()}</p>
-                </div>
+    <div className="flex flex-wrap justify-around">
+      {loading && <p className="loading">Loading...</p>}
+      {error && <p className="error">{error}</p>}
+      {articles?.map((article) => {
+        const date = article.date
+          ? new Date(article.date.seconds * 1000)
+          : new Date();
+        return (
+          <div className="m-4 bg-opacity-10 rounded-lg w-[300px] border border-[#30D1FF]">
+            <img
+              src={article.image || "/assets/images/space.png"}
+              alt={article.title}
+            />
+            <div className="p-4 border-t border-[#30D1FF]">
+              <a href={`/blog/${article.id}`}>
+                <h2 className="title text-xl text-[#30D1FF] mb-2">
+                  {article.title}
+                </h2>
+              </a>
+              <p className="text-sm mb-2">
+                {article.summary.slice(0, 120)} ...
+              </p>
+              <div className="flex justify-between">
+                <p className="text-xs text-[#30D1FF]">3 min</p>
+                <p className="text-xs text-[#30D1FF]">{date.toDateString()}</p>
               </div>
             </div>
-          );
-        })}
-      </div>
-    </>
+          </div>
+        );
+      })}
+    </div>
   );
 };
 
