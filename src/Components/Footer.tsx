8e5ec06ad9e348218de0ac0a670fe69687d05742
heluansantos <heluan.caetano@gmail.com>
2023-08-26 17:46:04 +0200

    feat: Improvments blog footer component

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,26 +1,42 @@
 export function Footer() {
+  const footerInfo = [
+    {
+      id: 0,
+      title: "About Us",
+      action: "#/privacy-policy",
+    },
+    {
+      id: 1,
+      title: "FAQs",
+      action: "#/privacy-policy",
+    },
+    {
+      id: 2,
+      title: "Contact Us",
+      action: "#/privacy-policy",
+    },
+  ];
+
   return (
-    <div className="flex w-full bottom-0 justify-between items-center absolute pb-4">
-      <img src="logos/logo.png" alt="Logo Cosmic Exodus" />
-      <div className="flex w-full justify-evenly items-center">
-        <a
-          href="#/privacy-policy"
-          className="text-[#A3A9F6] text-xs hover:text-[#30D1FF]"
-        >
-          About Us
-        </a>
-        <a
-          href="#/privacy-policy"
-          className="text-[#A3A9F6] text-xs hover:text-[#30D1FF]"
-        >
-          FAQs
-        </a>
-        <a
-          href="#/privacy-policy"
-          className="text-[#A3A9F6] text-xs hover:text-[#30D1FF]"
-        >
-          Contact Us
-        </a>
+    <div className="flex w-full bottom-0 absolute pb-4 flex-col">
+      <div className="flex justify-between items-center">
+        <img src="logos/logo.png" alt="Logo Cosmic Exodus" />
+        <div className="flex w-full justify-evenly items-center">
+          {footerInfo?.map((item, index) => (
+            <a
+              href={item.action}
+              className="text-[#A3A9F6] text-sm hover:text-[#30D1FF] font-bold"
+              key={index}
+            >
+              {item.title}
+            </a>
+          ))}
+        </div>
+      </div>
+      <div className="w-full flex justify-center items-center mt-8">
+        <span className="text-xs">
+          © 2023. Cosmic Exodus, All rights reserved.{" "}
+        </span>
       </div>
     </div>
   );
