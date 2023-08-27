2bc21f3aa1881c34c6524406754f82936efc3a18
heluansantos <heluan.caetano@gmail.com>
2023-08-27 02:11:09 +0200

    feat: Verification wallet

--- src/Context/APIProvider.tsx
+++ src/Context/APIProvider.tsx
@@ -2,61 +2,65 @@ import { createContext, useContext, useState } from "react";
 import { FirebaseContext } from "./FirebaseProvider";
 import { collection, getDoc, getDocs, doc } from "firebase/firestore";
 
-
 interface APIContextProps {
-    signWaitlist: (email: string) => Promise<boolean>;
-    signup: (name: string, email: string, password: string) => Promise<boolean>;
-
+  signWaitlist: (email: string) => Promise<boolean>;
+  signup: (name: string, email: string, password: string) => Promise<boolean>;
 }
 
 export const APIContext = createContext<APIContextProps>({} as APIContextProps);
 
-const APIProvider = ({children}: any) => {
-    const [uri, setUri] = useState('https://us-central1-cosmosexodus.cloudfunctions.net/api/v1')
+const APIProvider = ({ children }: any) => {
+  const [uri] = useState(
+    "https://us-central1-cosmosexodus.cloudfunctions.net/api/v1"
+  );
 
-    const post = async (endpoint:string, data:any) => {
-        try {
-            await fetch(`${uri}/${endpoint}`, {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify(data)
-            }).then(res => {
-                if(res.status === 200){
-                    return true;
-                }
-
-                return false
-            })
-        }catch(e: any) {
-            return false;
+  const post = async (endpoint: string, data: any) => {
+    try {
+      await fetch(`${uri}/${endpoint}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      }).then((res) => {
+        if (res.status === 200) {
+          return true;
         }
 
-        return true;
+        return false;
+      });
+    } catch (e: any) {
+      return false;
     }
 
-    const signWaitlist = async (email: string) => {
-        return post("signWaitlist", {
-            email
-        })
-    }
-    const signup = async (name: string, email: string, password: string) => {
-        return post("signup",{
-            name, 
-            email, 
-            password,
-            gdpr: true
-        })
-    }
+    return true;
+  };
 
-    return <APIContext.Provider value={{
+  const signWaitlist = async (email: string) => {
+    return post("signWaitlist", {
+      email,
+    });
+  };
+  const signup = async (name: string, email: string, password: string) => {
+    return post("signup", {
+      name,
+      email,
+      password,
+      gdpr: true,
+    });
+  };
+
+  return (
+    <APIContext.Provider
+      value={{
         signWaitlist,
-        signup
-    }}> {children} </APIContext.Provider>
-}
+        signup,
+      }}
+    >
+      {" "}
+      {children}{" "}
+    </APIContext.Provider>
+  );
+};
 
 export default APIProvider;
-        
-
-        
--- src/Pages/Invest.tsx
+++ src/Pages/Invest.tsx
@@ -1,9 +1,10 @@
 import { useMemo, useState } from "react";
 import { Footer } from "../Components/Footer";
+import { useAddress } from "@thirdweb-dev/react";
 
 const Invest = () => {
+  const address = useAddress();
   const [amount, setAmount] = useState<string>();
-  const [email, setEmail] = useState<string>();
   const ref = useMemo(() => {
     return `https://app.kado.money/?onPayCurrency=USD&onPayAmount=${amount}&onRevCurrency=USDC&offPayCurrency=USDC&offRevCurrency=USD&network=OSMOSIS&onToAddress=X&offFromAddress=X&cryptoList=USDC&networkList=OSMOSIS&apiKey=API_KEY&product=BUY`;
   }, [amount]);
@@ -11,43 +12,36 @@ const Invest = () => {
   return (
     <div className="min-h-full justify-center items-center flex flex-col ">
       <div className="flex justify-center items-center">
-        <form>
-          <div className="mb-4">
+        {address ? (
+          <form>
             <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-              Email address
+              Insert amount
             </label>
-            <input
-              type="email"
-              id="email"
-              onChange={(e) => setEmail(e.target.value)}
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="john.doe@company.com"
-              required
-            />
-          </div>
-          <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-            Insert amount
-          </label>
-          <div className="flex">
-            <input
-              type="number"
-              id="invest"
-              onChange={(e) => setAmount(e.target.value)}
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="100.000,00"
-              required
-            />
-            <a
-              target="_blank"
-              rel="noreferrer"
-              href={ref}
-              type="submit"
-              className="text-white ml-4 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            >
-              Invest
-            </a>
-          </div>
-        </form>
+            <div className="flex">
+              <input
+                type="number"
+                id="invest"
+                onChange={(e) => setAmount(e.target.value)}
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="100.000,00"
+                required
+              />
+              <a
+                target="_blank"
+                rel="noreferrer"
+                href={ref}
+                type="submit"
+                className="text-white ml-4 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                Invest
+              </a>
+            </div>
+          </form>
+        ) : (
+          <span className="block mt-12 text-sm font-medium text-gray-900 dark:text-white">
+            Conect your wallet to continue
+          </span>
+        )}
       </div>
 
       {Footer()}
