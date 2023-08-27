dd310fe90aecfa608387a403b02a3057424f7a07
heluansantos <heluan.caetano@gmail.com>
2023-08-27 13:53:37 +0200

    feat: Social networks component

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,14 +1,19 @@
+import { SocialNetworks } from "./SocialNetworks";
+
 export function Footer() {
   return (
-    <div className="flex w-[94%] bottom-0 absolute flex-row justify-between border-t border-white pt-4 pb-8">
-      <div>
-        <span className="text-xs">
-          © 2023. Cosmic Exodus, All rights reserved.{" "}
-        </span>
-      </div>
-      <div>
-        <img src="logos/logo.png" alt="Logo Cosmic Exodus" />
-      </div>
-    </div>
+    <>
+      {SocialNetworks()}
+      <div className="flex w-[94%] bottom-0 absolute flex-row justify-between border-t border-white pt-4 pb-8">
+        <div>
+          <span className="text-xs">
+            © 2023. Cosmic Exodus, All rights reserved.{" "}
+          </span>
+        </div>
+        <div>
+          <img src="logos/logo.png" alt="Logo Cosmic Exodus" />
+        </div>
+      </div>{" "}
+    </>
   );
 }
---
+++ src/Components/SocialNetworks.tsx
@@ -0,0 +1,47 @@
+export function SocialNetworks() {
+  const socialNetworks = [
+    {
+      title: "X",
+      img: "logos/mdi_x.png",
+      action: "https://x.com/Cosmic_Exodus",
+    },
+    {
+      title: "LinkdIn",
+      img: "logos/ri_linkedin-fill.png",
+      action: "https://www.linkedin.com/company/cosmic-exodus/",
+    },
+    {
+      title: "Youtube",
+      img: "logos/mdi_youtube.png",
+      action: "https://www.youtube.com/@cosmicexodus",
+    },
+    {
+      title: "Discord",
+      img: "logos/ic_baseline-discord.png",
+      action: "https://discord.com/invite/36AdSFvSX5",
+    },
+  ];
+
+  return (
+    <div className="flex w-full justify-center items-center flex-col py-12 absolute bottom-32">
+      <h1 className="mb-16 text-3xl font-semibold text-center">
+        Support us in social media
+      </h1>
+      <div className="flex items-center justify-around">
+        {socialNetworks?.map((social, index) => {
+          return (
+            <a
+              href={social.action}
+              target="_blank"
+              rel="noreferrer"
+              key={index}
+              className="mx-4"
+            >
+              <img src={social.img} alt={social.title} />
+            </a>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
