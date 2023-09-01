f87ffd28e4c863d1d16b3ac2061be4485b39a86d
heluansantos <heluan.caetano@gmail.com>
2023-09-01 12:19:44 +0200

    feat: Improvments inputs default

--- src/Pages/Deck.tsx
+++ src/Pages/Deck.tsx
@@ -2,40 +2,68 @@ import { useMemo, useState } from "react";
 import { Footer } from "../Components/Footer";
 
 const Deck = () => {
-  const [email, setEmail] = useState<string>();
+  const [email, setEmail] = useState("");
+  const [signedUp, setSignedUp] = useState(false);
+  const [error, setError] = useState("");
   const ref = useMemo(() => {
     return "https://drive.google.com/file/d/1h1HENakcd5-ViSZnVAwpZs_FU3ncyCHQ/view";
   }, []);
 
-  return (
-    <div className="min-h-full justify-center items-center flex flex-col ">
-      <div className="flex justify-center items-center">
-        <form>
-          <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-            Insert your E-mail
-          </label>
-          <div className="flex">
-            <input
-              type="email"
-              id="email"
-              onChange={(e) => setEmail(e.target.value)}
-              className="focus:outline-0 border border-[#2ed2ff] bg-transparent text-gray-200 text-sm rounded-lg focus:ring-blue-500 block w-full p-2.5 "
-              placeholder="joao@email.com"
-              required
-            />
-            <a
-              target="_blank"
-              rel="noreferrer"
-              href={ref}
-              type="submit"
-              className="text-white ml-4 border-[#2ed2ff] border hover:bg-sky-900 focus:ring-4 focus:outline-none focus:ring-sky-600 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center "
-            >
-              Open
-            </a>
-          </div>
-        </form>
-      </div>
+  const submitForm = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+
+    if (email) {
+      // open deck
+    } else {
+      // add class to gdpr label .error
+    }
+  };
+
+  const form = (
+    <>
+      <p>
+        Insert your E-mail.
+        <br />
+        For open our Cosmic Deck!
+      </p>
+
+      <form>
+        <input
+          type="text"
+          placeholder="Email Address"
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />
+
+        <button type="submit" onClick={submitForm}>
+          Open deck
+        </button>
+      </form>
+    </>
+  );
+
+  const success = (
+    <div className="success">
+      <h2>Thank you for open Cosmic deck!</h2>
     </div>
+  );
+
+  const failure = (
+    <div className="failure">
+      <h2>Something went wrong!</h2>
+      <p>{error}</p>
+    </div>
+  );
+
+  return (
+    <>
+      <div className="waitlist">
+        <h1>Cosmic Deck</h1>
+        {signedUp ? success : error ? failure : form}
+      </div>
+    </>
   );
 };
 
--- src/Pages/Invest.tsx
+++ src/Pages/Invest.tsx
@@ -3,46 +3,75 @@ import { useAddress } from "@thirdweb-dev/react";
 
 const Invest = () => {
   const address = useAddress();
+  const [signedUp, setSignedUp] = useState(false);
+  const [error, setError] = useState("");
   const [amount, setAmount] = useState<string>();
   const ref = useMemo(() => {
     return `https://app.kado.money/?onPayCurrency=USD&onPayAmount=${amount}&onRevCurrency=USDC&offPayCurrency=USDC&offRevCurrency=USD&network=OSMOSIS&onToAddress=X&offFromAddress=X&cryptoList=USDC&networkList=OSMOSIS&apiKey=API_KEY&product=BUY`;
   }, [amount]);
 
-  return (
-    <div className="min-h-full justify-center items-center flex flex-col ">
-      <div className="flex justify-center items-center">
-        {address ? (
-          <form>
-            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-              Insert amount
-            </label>
-            <div className="flex">
-              <input
-                type="number"
-                id="invest"
-                onChange={(e) => setAmount(e.target.value)}
-                className="focus:outline-0 border border-[#2ed2ff] bg-transparent text-gray-200 text-sm rounded-lg focus:ring-blue-500 block w-full p-2.5 "
-                placeholder="100.000,00"
-                required
-              />
-              <a
-                target="_blank"
-                rel="noreferrer"
-                href={ref}
-                type="submit"
-                className="text-white ml-4 border-[#2ed2ff] border hover:bg-sky-900 focus:ring-4 focus:outline-none focus:ring-sky-600 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center "
-              >
-                Invest
-              </a>
-            </div>
-          </form>
-        ) : (
-          <span className="block mt-12 text-sm font-medium text-gray-900 dark:text-white">
-            Conect your wallet to continue
-          </span>
-        )}
-      </div>
+  const submitForm = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+
+    if (amount) {
+      // open invest
+    } else {
+      // add class to gdpr label .error
+    }
+  };
+
+  const form = (
+    <>
+      <p>
+        Insert amount.
+        <br />
+        For open investment dashboard!
+      </p>
+
+      <form>
+        <input
+          type="text"
+          placeholder="100.000,00"
+          value={amount}
+          onChange={(e) => {
+            setAmount(e.target.value);
+          }}
+        />
+
+        <button type="submit" onClick={submitForm}>
+          Invest
+        </button>
+      </form>
+    </>
+  );
+
+  const success = (
+    <div className="success">
+      <h2>Thank you for open Cosmic deck!</h2>
     </div>
+  );
+
+  const failure = (
+    <div className="failure">
+      <h2>Something went wrong!</h2>
+      <p>{error}</p>
+    </div>
+  );
+
+  const conectWallet = (
+    <div className="failure">
+      <h2>Conect your wallet to continue!</h2>
+      <p>{error}</p>
+    </div>
+  );
+
+  return (
+    <>
+      <div className="waitlist">
+        <h1>Cosmic Deck</h1>
+        {signedUp ? success : error ? failure : !address ? conectWallet : form}
+      </div>
+    </>
   );
 };
 
