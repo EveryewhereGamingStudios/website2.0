2b36d5e7f0970280d990ac882c78f07508e66e0a
heluansantos <heluan.caetano@gmail.com>
2023-09-07 18:47:13 +0200

    feat: Create a new article structure Js

--- src/Pages/CreateArticle.tsx
+++ src/Pages/CreateArticle.tsx
@@ -1,10 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 
 export default function CreateArticle() {
   const [articleData, setArticleData] = useState({
-    mainTitle: "",
+    articleTitle: "",
     bannerImage: "",
     link: "",
+    date: new Date().getDate(),
+    timeToRead: "",
     content: [
       {
         title: "",
@@ -13,44 +15,49 @@ export default function CreateArticle() {
     ],
   });
 
-  const handleInputChange = (e: { target: { name: any; value: any } }) => {
-    const { name, value } = e.target;
-    setArticleData({ ...articleData, [name]: value });
-  };
+  const handleInputChange = useCallback(
+    (e: { target: { name: any; value: any } }) => {
+      const { name, value } = e.target;
+      setArticleData({ ...articleData, [name]: value });
+    },
+    [articleData]
+  );
 
-  const handleContentChange = (
-    index: number,
-    e:
-      | React.ChangeEvent<HTMLInputElement>
-      | React.ChangeEvent<HTMLTextAreaElement>
-  ) => {
-    const { name, value } = e.target;
-    const updatedContent = [...articleData.content];
-    updatedContent[index] = { ...updatedContent[index], [name]: value };
-    setArticleData({ ...articleData, content: updatedContent });
-  };
+  const handleContentChange = useCallback(
+    (index: any, e: any) => {
+      const { name, value } = e.target;
+      const updatedContent = [...articleData.content];
+      updatedContent[index] = { ...updatedContent[index], [name]: value };
+      setArticleData({ ...articleData, content: updatedContent });
+    },
+    [articleData]
+  );
 
-  const addContentItem = () => {
+  const addContentItem = useCallback(() => {
     setArticleData({
       ...articleData,
       content: [...articleData.content, { title: "", description: "" }],
     });
-  };
+  }, [articleData]);
 
-  const removeContentItem = (index: number) => {
-    const updatedContent = [...articleData.content];
-    updatedContent.splice(index, 1);
-    setArticleData({ ...articleData, content: updatedContent });
-  };
+  const removeContentItem = useCallback(
+    (index: number) => {
+      const updatedContent = [...articleData.content];
+      updatedContent.splice(index, 1);
+      setArticleData({ ...articleData, content: updatedContent });
+    },
+    [articleData]
+  );
 
-  const handleSubmit = (e: { preventDefault: () => void }) => {
+  const handleSubmit = useCallback((e: { preventDefault: () => void }) => {
     e.preventDefault();
-    // Call the function to save the new article with articleData
-    // For example: addArticle(articleData);
+
     try {
       setArticleData({
-        mainTitle: "",
+        articleTitle: "",
         bannerImage: "",
+        timeToRead: "",
+        date: new Date().getDate(),
         link: "",
         content: [
           {
@@ -59,66 +66,102 @@ export default function CreateArticle() {
           },
         ],
       });
-    } catch {}
-  };
+    } catch (error) {
+      console.error("Error saving article:", error);
+    }
+  }, []);
 
   return (
     <div className="w-screen h-full min-h-screen mx-auto px-32 pt-8 p-4 bg-sky-950">
       <h1 className="text-2xl font-bold mb-4">Create a New Article</h1>
       <form onSubmit={handleSubmit}>
-        {/* Main Title */}
-        <div className="mb-4">
-          <label className="block text-sm font-semibold">Main Title:</label>
-          <input
-            type="text"
-            name="mainTitle"
-            value={articleData.mainTitle}
-            onChange={handleInputChange}
-            className="border border-gray-300 p-2 w-full rounded"
-          />
-        </div>
-
-        {/* Banner Image */}
-        <div className="mb-4">
-          <label className="block text-sm font-semibold">
-            Banner Image URL:
-          </label>
-          <input
-            type="text"
-            name="bannerImage"
-            value={articleData.bannerImage}
-            onChange={handleInputChange}
-            className="border border-gray-300 p-2 w-full rounded"
-          />
-        </div>
-
-        {/* Link */}
-        <div className="mb-4">
-          <label className="block text-sm font-semibold">Link:</label>
-          <input
-            type="text"
-            name="link"
-            value={articleData.link}
-            onChange={handleInputChange}
-            className="border border-gray-300 p-2 w-full rounded"
-          />
-        </div>
-
-        {/* Content */}
-        {articleData.content.map((contentItem, index) => (
-          <div key={index} className="mb-4">
-            <label className="block text-sm font-semibold">Title:</label>
+        <div className="flex flex-row justify-between w-full flex-wrap">
+          <div className="mb-4">
+            <label
+              htmlFor="articleTitle"
+              className="block text-sm font-semibold"
+            >
+              Article Title:
+            </label>
             <input
               type="text"
+              id="articleTitle"
+              name="articleTitle"
+              value={articleData.articleTitle}
+              onChange={handleInputChange}
+              className="border border-gray-300 p-2 w-full rounded"
+            />
+          </div>
+
+          <div className="mb-4">
+            <label
+              htmlFor="bannerImage"
+              className="block text-sm font-semibold"
+            >
+              Banner Image URL:
+            </label>
+            <input
+              type="text"
+              id="bannerImage"
+              name="bannerImage"
+              value={articleData.bannerImage}
+              onChange={handleInputChange}
+              className="border border-gray-300 p-2 w-full rounded"
+            />
+          </div>
+
+          <div className="mb-4">
+            <label htmlFor="link" className="block text-sm font-semibold">
+              Link:
+            </label>
+            <input
+              type="text"
+              id="link"
+              name="link"
+              value={articleData.link}
+              onChange={handleInputChange}
+              className="border border-gray-300 p-2 w-full rounded"
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="timeToRead" className="block text-sm font-semibold">
+              Time to Read Article:
+            </label>
+            <input
+              type="text"
+              id="timeToRead"
+              name="timeToRead"
+              value={articleData.timeToRead}
+              onChange={handleInputChange}
+              className="border border-gray-300 p-2 w-full rounded"
+            />
+          </div>
+        </div>
+
+        {articleData.content.map((contentItem, index) => (
+          <div key={index} className="mb-4">
+            <label
+              htmlFor={`contentTitle-${index}`}
+              className="block text-sm font-semibold"
+            >
+              Title of paragraph {index + 1}:
+            </label>
+            <input
+              type="text"
+              id={`contentTitle-${index}`}
               name="title"
               value={contentItem.title}
               onChange={(e) => handleContentChange(index, e)}
               className="border border-gray-300 p-2 w-full rounded"
             />
-            <label className="block text-sm font-semibold mt-2">
+            <label
+              htmlFor={`contentDescription-${index}`}
+              className="block text-sm font-semibold mt-2"
+            >
               Description:
             </label>
             <textarea
+              id={`contentDescription-${index}`}
               name="description"
               value={contentItem.description}
               onChange={(e) => handleContentChange(index, e)}
