dc56540838b1b2baf45a39889b49f5e7d7dce392
heluansantos <heluan.caetano@gmail.com>
2023-09-15 15:57:38 +0200

    Improvments

--- src/Components/Nav.tsx
+++ src/Components/Nav.tsx
@@ -16,12 +16,12 @@ const Nav = () => {
         </div>
 
         <div className="nav-discord desktop-only items-center w-[170px] flex justify-end">
-          <a href={links.discord} className="flex">
+          <a href={links.discord} className="flex items-center">
             Join us on Discord
             <img
               src="/assets/images/discord.png"
               alt="Discord Channel icon"
-              className="ml-2 h-5 w-5"
+              className="ml-2 h-4 w-4"
             />
           </a>
         </div>
