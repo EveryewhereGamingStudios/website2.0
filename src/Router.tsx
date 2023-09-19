a04e46d1f27dfc030683872343097762df1a41c2
heluansantos <heluan.caetano@gmail.com>
2023-09-19 23:05:33 +0200

    Admin layout removed

--- src/Router.tsx
+++ src/Router.tsx
@@ -5,7 +5,6 @@ import Waitlist from "./Pages/Waitlist";
 import Signup from "./Pages/Signup";
 import Blog from "./Pages/Blog";
 import BlogArticle from "./Pages/BlogArticle";
-import AdminLayout from "./Layouts/AdminLayout";
 import PrivacyPolicy from "./Pages/PrivacyPolicy";
 import InGameActionsLayout from "./Layouts/InGameActionsLayout";
 import GetNFT from "./Pages/GetNFT";
@@ -80,16 +79,6 @@ const router = createBrowserRouter([
             <Footer />
           </div>
         ),
-      },
-    ],
-  },
-  {
-    path: "/admin",
-    element: <AdminLayout />,
-    children: [
-      {
-        path: "",
-        element: <h1>Hello</h1>,
       },
     ],
   },
