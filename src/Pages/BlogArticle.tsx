26b6a29c4d5e7035118dcf0a4c8efd74ac11c30d
heluansantos <heluan.caetano@gmail.com>
2023-10-02 22:00:20 +0100

    External link on article

--- src/Pages/BlogArticle.tsx
+++ src/Pages/BlogArticle.tsx
@@ -7,6 +7,7 @@ import { useBlog } from "../Context/BlogProvider";
 import { CardBlog } from "../Components/CardBlog";
 
 export const EmojiTextWithLineBreak = ({ text }: any) => {
+  const urlRegex = /(https?:\/\/\S+|www\.\S+)/gi;
   const textParts = text.split("<br />");
 
   return (
@@ -23,11 +24,42 @@ export const EmojiTextWithLineBreak = ({ text }: any) => {
             | null
             | undefined,
           index: any
-        ) => (
-          <React.Fragment key={index}>
-            {index > 0 && <br />} {part}
-          </React.Fragment>
-        )
+        ) => {
+          if (typeof part === "string") {
+            const segments = part.split(urlRegex);
+
+            const renderedSegments = segments.map((segment, i) => {
+              if (segment.match(urlRegex)) {
+                return (
+                  <a
+                    key={i}
+                    href={`https://${segment}`}
+                    target="_blank"
+                    className="text-sky-400 font-bold"
+                    rel="noreferrer"
+                  >
+                    {segment}
+                  </a>
+                );
+              } else {
+                return segment;
+              }
+            });
+
+            return (
+              <React.Fragment key={index}>
+                {index > 0 && <br />}
+                {renderedSegments}
+              </React.Fragment>
+            );
+          }
+
+          return (
+            <React.Fragment key={index}>
+              {index > 0 && <br />} {part}
+            </React.Fragment>
+          );
+        }
       )}
     </span>
   );
