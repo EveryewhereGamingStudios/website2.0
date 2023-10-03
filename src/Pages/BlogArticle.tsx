3c354fe17645d1440926cdd6e1ba7cded33ea283
heluansantos <heluan.caetano@gmail.com>
2023-10-03 18:34:05 +0100

    Utils blog

--- src/Pages/BlogArticle.tsx
+++ src/Pages/BlogArticle.tsx
@@ -3,67 +3,8 @@ import { useEffect, useMemo } from "react";
 import { Helmet } from "react-helmet";
 import { useParams } from "react-router-dom";
 import { useBlog } from "../Context/BlogProvider";
-// import sanitizeHtml from "sanitize-html";
 import { CardBlog } from "../Components/CardBlog";
-
-export const EmojiTextWithLineBreak = ({ text }: any) => {
-  const urlRegex = /(https?:\/\/\S+|www\.\S+)/gi;
-  const textParts = text.split("<br />");
-
-  return (
-    <span>
-      {textParts.map(
-        (
-          part:
-            | string
-            | number
-            | boolean
-            | React.ReactElement<any, string | React.JSXElementConstructor<any>>
-            | Iterable<React.ReactNode>
-            | React.ReactPortal
-            | null
-            | undefined,
-          index: any
-        ) => {
-          if (typeof part === "string") {
-            const segments = part.split(urlRegex);
-
-            const renderedSegments = segments.map((segment, i) => {
-              if (segment.match(urlRegex)) {
-                return (
-                  <a
-                    key={i}
-                    href={`https://${segment}`}
-                    target="_blank"
-                    className="text-sky-400 font-bold"
-                    rel="noreferrer"
-                  >
-                    {segment}
-                  </a>
-                );
-              } else {
-                return segment;
-              }
-            });
-
-            return (
-              <React.Fragment key={index}>
-                {index > 0 && <br />}
-                {renderedSegments}
-              </React.Fragment>
-            );
-          }
-
-          return (
-            <React.Fragment key={index}>
-              {index > 0 && <br />} {part}
-            </React.Fragment>
-          );
-        }
-      )}
-    </span>
-  );
-};
+import { EmojiTextWithLineBreak } from "../utils/blog";
 
 export default function BlogArticle() {
   const { id } = useParams<{ id: string }>();
