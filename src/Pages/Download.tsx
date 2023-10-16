dc790054f0a54c9d34f8e5722a0cc542cb164219
heluansantos <heluan.caetano@gmail.com>
2023-10-16 20:44:05 +0100

    Download page

--- src/Pages/Download.tsx
+++ src/Pages/Download.tsx
@@ -1,32 +1,75 @@
+import { useCallback, useState } from "react";
+
 const Download = () => {
+  const [checked, setChecked] = useState(false);
+
+  const handleLinkHover = useCallback(() => {
+    if (!checked) {
+      alert("Please check the checkbox to download.");
+    }
+  }, [checked]);
+
+  const handleChange = useCallback(() => {
+    setChecked(!checked);
+  }, [checked]);
+
   return (
-    <div className="flex flex-col items-center justify-center">
-      <div className="flex flex-col items-center justify-center">
-        <p className="text-center">
-          This is an Alpha version of our launcher. Windows will try to warn you
-          about the .exe but fear not.
-          <br />
-          <br />
-          Install the launcher to access the game.
-        </p>
-        <div className="w-full pb-8 text-center border-gray-700 border-b-[0.5px] mt-8">
+    <dl className="transition-all ease-in-out -my-3 divide-y divide-sky-900 text-sm">
+      <div className="flex flex-col items-center justify-between min-h-[80vh]">
+        <div />
+
+        <div className="items-center justify-center flex flex-col">
+          <p className="text-start p-4">
+            <p className="text-lg">Warning: </p>
+            As of today we do not have Microsoft Credentials. This will make our
+            launcher flag Windows Defender.
+            <br />
+            We understand this causes trust issues and people are afraid of
+            installing it, due to virus/malware concerns.
+            <br />
+            There is not much we can do about it right now, so please understand
+            and have some patience with us.
+          </p>
+
+          <div className="flex items-center w-full mb-4 px-4">
+            <input
+              id="default-checkbox"
+              type="checkbox"
+              checked={checked}
+              onChange={handleChange}
+            />
+            <label
+              htmlFor="default-checkbox"
+              className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+            >
+              I understand, download anyway.
+            </label>
+          </div>
+
+          <img
+            src="/assets/images/download-avatar.png"
+            alt="Saga logo"
+            className="md:h-[300px] h-[200px] my-8"
+          />
+
           <a
-            target="_blank"
-            rel="noreferrer"
-            href="https://firebasestorage.googleapis.com/v0/b/cosmic-exodus.appspot.com/o/launcher%2FCosmic%20Launcher%20Installer.exe?alt=media&token=0737da76-40f8-452e-869c-b4f737be79ed"
-            className="mx-4 border-2 border-[#2ed2ff] text-[#2ed2ff] p-[5.5px] px-[20px] rounded-[10px] mt-6"
+            href={
+              checked
+                ? "https://firebasestorage.googleapis.com/v0/b/cosmic-exodus.appspot.com/o/launcher%2FCosmic%20Launcher%20Installer.exe?alt=media&token=0737da76-40f8-452e-869c-b4f737be79ed"
+                : "#/Download%20launcher"
+            }
+            onMouseOver={handleLinkHover}
+            className={` ${
+              checked ? "animate-pulse" : ""
+            }  mx-4 border-2 border-[#2ed2ff] text-[#2ed2ff] p-[5.5px] px-[20px] rounded-[10px]`}
           >
-            Download launcher
+            Download
           </a>
         </div>
-        <a
-          href="/"
-          className="mx-4 border-2 border-[#2ed2ff] text-[#2ed2ff] p-[5.5px] px-[20px] rounded-[10px] mt-6"
-        >
-          Profile
-        </a>
+
+        <div />
       </div>
-    </div>
+    </dl>
   );
 };
 
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,25 +1,13 @@
 import { ConnectWallet, useAddress } from "@thirdweb-dev/react";
 import links from "../data/links.json";
-import { useCallback, useState } from "react";
+import { useState } from "react";
 import { Profile } from "./Profile";
-import { useFirebase } from "../Context/FirebaseProvider";
 import Referral from "./Referral";
+import Download from "./Download";
 
 const Home = () => {
   const address = useAddress();
-  const { users } = useFirebase();
   const [selectedTab, setSelectedTab] = useState("accounts");
-  const [checked, setChecked] = useState(false);
-
-  const handleLinkHover = useCallback(() => {
-    if (!checked) {
-      alert("Please check the checkbox to download.");
-    }
-  }, [checked]);
-
-  const handleChange = useCallback(() => {
-    setChecked(!checked);
-  }, [checked]);
 
   return (
     <>
@@ -101,62 +89,7 @@ const Home = () => {
               </dl>
             </div>
           ) : selectedTab === "download" ? (
-            <dl className="transition-all ease-in-out -my-3 divide-y divide-sky-900 text-sm">
-              <div className="flex flex-col items-center justify-between min-h-[80vh]">
-                <div />
-
-                <div className="items-center justify-center flex flex-col">
-                  <p className="text-start p-4">
-                    <p className="text-lg">Warning: </p>
-                    As of today we do not have Microsoft Credentials. This will
-                    make our launcher flag Windows Defender.
-                    <br />
-                    We understand this causes trust issues and people are afraid
-                    of installing it, due to virus/malware concerns.
-                    <br />
-                    There is not much we can do about it right now, so please
-                    understand and have some patience with us.
-                  </p>
-
-                  <div className="flex items-center w-full mb-4 px-4">
-                    <input
-                      id="default-checkbox"
-                      type="checkbox"
-                      checked={checked}
-                      onChange={handleChange}
-                    />
-                    <label
-                      htmlFor="default-checkbox"
-                      className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                    >
-                      I understand, download anyway.
-                    </label>
-                  </div>
-
-                  <img
-                    src="/assets/images/download-avatar.png"
-                    alt="Saga logo"
-                    className="md:h-[300px] h-[200px] my-8"
-                  />
-
-                  <a
-                    href={
-                      checked
-                        ? "https://firebasestorage.googleapis.com/v0/b/cosmic-exodus.appspot.com/o/launcher%2FCosmic%20Launcher%20Installer.exe?alt=media&token=0737da76-40f8-452e-869c-b4f737be79ed"
-                        : "#/Download%20launcher"
-                    }
-                    onMouseOver={handleLinkHover}
-                    className={` ${
-                      checked ? "animate-pulse" : ""
-                    }  mx-4 border-2 border-[#2ed2ff] text-[#2ed2ff] p-[5.5px] px-[20px] rounded-[10px]`}
-                  >
-                    Download
-                  </a>
-                </div>
-
-                <div />
-              </div>
-            </dl>
+            <Download />
           ) : selectedTab === "referral" ? (
             <Referral />
           ) : null}
